251156ac8c5b894978ef3ab437357523117bc686
jch026 <jch026@ucsd.edu>
2018-08-15 16:48:27 -0700

    Initial Commit

---
+++ LugAway App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LugAway App
+//
+//  Created by Joseph Ho on 8/14/18.
+//  Copyright © 2018 broseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
